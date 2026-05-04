b8f5245eeb3554548777cfc24348120520cbf354
AnkitGang <ankitgang7777@gmail.com>
2026-05-05 00:13:28 +0530

    Organize project structure

---
+++ src/RAG_based_Student_Manager/main.py
@@ -0,0 +1,45 @@
+import sys
+from src.RAG_based_Student_Manager.services.student_service import add_student, view_student, delete_student, search_student, fetch_github_profile
+import logging
+
+logging.basicConfig(
+    filename="../../logs/app.log",
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s - %(message)s"
+)
+
+
+def main():
+    while True:
+        print("\n1. Add Student")
+        print("2. View Students")
+        print("3. Delete Student")
+        print("4. Search Student")
+        print("5. Fetch GitHub Profile")
+        print("6. Exit")
+
+        try:
+            choice = int(input("Enter choice: "))
+        except ValueError as e:
+            logging.error("Invalid input!!!!!!")
+            # print("Invalid input!!!!!!")
+            continue
+
+        if choice == 1:
+            add_student()
+        elif choice == 2:
+            view_student()
+        elif choice == 3:
+            delete_student()
+        elif choice == 4:
+            search_student()
+        elif choice == 5:
+            fetch_github_profile()
+        else:
+            logging.info("Exiting !!!!!!!!!!!")
+            # print("Exiting !!!!!!!!!!!")
+            sys.exit()
+
+
+if __name__ == "__main__":
+    main()
---
+++ src/RAG_based_Student_Manager/services/rag_service.py
@@ -0,0 +1,85 @@
+from src.RAG_based_Student_Manager.utils.file_handler import load_students
+from src.RAG_based_Student_Manager.services.embedding_service import get_embeddings
+import numpy as np
+
+
+def build_index():
+    students = load_students()
+
+    if not students:
+        return None
+
+    index = []
+
+    for student in students:
+        text = student_to_text(student)
+
+        if not text:
+            continue
+
+        embedding = get_embeddings(text)
+
+        if embedding:
+            index.append({
+                "student": student,
+                "text": text,
+                "embedding": embedding
+            })
+
+    return index
+
+
+def retrieve_relevant_info(query, index, top_k=2):
+    query_embedding = get_embeddings(query)
+
+    if not query_embedding:
+        return []
+
+    scores = []
+
+    for item in index:
+        similarity = cosine_similarity(query_embedding, item["embedding"])
+        scores.append((similarity, item))
+
+    print([(similarity, item["text"]) for similarity, item in scores])
+
+    scores.sort(reverse=True, key=lambda x: x[0])
+    return [item for _, item in scores[:top_k]]
+
+
+def cosine_similarity(a, b):
+    if a is None or b is None:
+        return 0
+
+    norm_a = np.linalg.norm(a)
+    norm_b = np.linalg.norm(b)
+
+    if norm_a == 0 or norm_b == 0:
+        return 0
+
+    return np.dot(a, b) / (norm_a * norm_b)
+
+
+def student_to_text(student):
+    if not student["name"]:
+        return None
+
+    if not student["age"]:
+        return None
+
+    github = student.get("github")
+
+    text = f"{student['name']} is {student['age']} years old. "
+
+    if github and isinstance(github, dict):
+        repos = github.get("repos", "")
+        followers = github.get("followers", "")
+        if repos is not None and repos != "" and followers is not None and followers != "":
+            text += f"He/She has {repos} repos and {followers} followers on GitHub.\n"
+        else:
+            text += "He/She does not have Github profile\n"
+    else:
+        text += "He/She does not have Github profile\n"
+
+    return text
+
